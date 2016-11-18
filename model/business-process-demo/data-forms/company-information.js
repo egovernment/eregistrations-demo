9d96492e89589f4eb8e8aec82ef849d3c0ea89e7
Mariusz Nowak <medyk@medikoo.com>
2016-11-18 08:39:36 -0600

    Split demo

--- model/business-process-demo/data-forms/company-information.js
+++ model/business-process-demo/data-forms/company-information.js
@@ -3,18 +3,32 @@
 var db                  = require('../../../db')
   , _                   = require('../../../i18n')
   , FormSection         = require('eregistrations/model/form-section')(db)
+  , FormSectionGroup    = require('eregistrations/model/form-section-group')(db)
   , BusinessProcessDemo = require('../fields');
 
 BusinessProcessDemo.prototype.dataForms.map.define('companyInformation', {
 	nested: true,
-	type: FormSection
+	type: FormSectionGroup
 });
 
 BusinessProcessDemo.prototype.dataForms.map.companyInformation.setProperties({
 	label: _("Company Information"),
-	actionUrl: 'company-information',
-	propertyNames: ['businessName', 'expectedIncome', 'address/country', 'address/city',
-		'address/street']
+	actionUrl: 'company-information'
 });
 
+var companyInformation = BusinessProcessDemo.prototype.dataForms.map.companyInformation;
+
+companyInformation.sections.defineProperties({
+	details: { type: FormSection, nested: true },
+	address: { type: FormSection, nested: true }
+});
+
+companyInformation.sections.details.setProperties({
+	propertyNames: []
+});
+
+companyInformation.sections.address.setProperties({
+	label: _("Address"),
+	propertyNames: []
+});
 module.exports = BusinessProcessDemo;
