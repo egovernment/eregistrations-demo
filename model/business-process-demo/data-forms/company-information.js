59d85ab45fb748e6620e18bb0e5dc65d73570832
kamsi <gruca.kamil@gmail.com>
2016-12-08 19:08:13 +0100

    Made companyInformation a group section

--- model/business-process-demo/data-forms/company-information.js
+++ model/business-process-demo/data-forms/company-information.js
@@ -2,18 +2,35 @@
 
 var db                  = require('../../../db')
   , _                   = require('../../../i18n')
+  , FormSectionGroup    = require('eregistrations/model/form-section-group')(db)
   , FormSection         = require('eregistrations/model/form-section')(db)
   , BusinessProcessDemo = require('../fields');
 
 BusinessProcessDemo.prototype.dataForms.map.define('companyInformation', {
 	nested: true,
-	type: FormSection
+	type: FormSectionGroup
 });
 
 BusinessProcessDemo.prototype.dataForms.map.companyInformation.setProperties({
 	label: _("Company Information"),
-	actionUrl: 'company-information',
-	propertyNames: ['businessName', 'address/country', 'address/city', 'address/street']
+	actionUrl: 'company-information'
+});
+
+var companyInformation = BusinessProcessDemo.prototype.dataForms.map.companyInformation;
+
+companyInformation.sections.defineProperties({
+	personalDetails: { type: FormSection, nested: true },
+	address: { type: FormSection, nested: true }
+});
+
+companyInformation.sections.personalDetails.setProperties({
+	label: _("Personal Details"),
+	propertyNames: ['businessName']
+});
+
+companyInformation.sections.address.setProperties({
+	label: _("Address"),
+	propertyNames: ['address/country', 'address/city', 'address/street']
 });
 
 module.exports = BusinessProcessDemo;
